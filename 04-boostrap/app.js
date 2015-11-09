711987bf2c57efbef7c9a75ec0e279e8c8a76042
meluk <kmelisu@hotmail.com>
2015-11-09 10:04:39 -0600

    04-boostrap

---
+++ 04-boostrap/app.js
@@ -0,0 +1,15 @@
+
+angular.module('bootstrap', ['ngRoute', 'bootstrap.controllers'])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/',{
+    	templateUrl: 'views/home.html',
+    	controller:'HomeController'
+    })
+    .when('/products',{
+    	templateUrl: 'views/products.html',
+    	controller:'ProductsController'
+    })
+    .otherwise({
+    	redirectTO: '/',
+    });
+}]);
